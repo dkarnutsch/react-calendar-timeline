ccaad60cf4e8435c12925f24ba555eac64c219c2
dkarnutsch <d.karnutsch@gmail.com>
2018-07-31 15:45:30 +0200

    fixed locale stuff which should be removed if deployed to npm

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import Timeline from './lib/Timeline'
+import moment from "moment";
+// TODO Remove me if production ready and deployed to npm
+import "moment/locale/de";
 
 export {
   default as TimelineMarkers
@@ -6,5 +9,8 @@ export {
 export { default as TodayMarker } from './lib/markers/public/TodayMarker'
 export { default as CustomMarker } from './lib/markers/public/CustomMarker'
 export { default as CursorMarker } from './lib/markers/public/CursorMarker'
+
+// TODO Remove me if production ready and deployed to npm
+moment.locale("de");
 
 export default Timeline
